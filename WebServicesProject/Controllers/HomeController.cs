9e85a76fc9f5be78618acce816a5ddef0f2a3404
Haritone <2003harik2003@gmail.com>
2022-10-13 20:59:47 +0300

    Init Commit

---
+++ WebServicesProject/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebServicesProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Brand()
+        {
+            return View();
+        }
+
+        public IActionResult Specials()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
